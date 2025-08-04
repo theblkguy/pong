ed6074562f97fc173d6d18aa9f7af475c7a1ee1f
theblkguy <allblk13@gmail.com>
2025-08-03 22:59:17 -0500

    Add/ One paddle, one ball

---
+++ main.py
@@ -0,0 +1,37 @@
+from settings import *
+from sprites import *
+
+
+class Game:
+  def __init__(self):
+    pygame.init()
+    self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('Pong')
+    self.clock = pygame.time.Clock()
+    self.running = True
+
+    # sprites
+    self.all_sprites = pygame.sprite.Group()
+    self.paddle_sprites = pygame.sprite.Group()
+    self.player = Player((self.all_sprites, self.paddle_sprites))
+    self.ball = Ball(self.all_sprites, self.paddle_sprites)
+
+  def run(self):
+    while self.running:
+      dt = self.clock.tick(60) / 1000  # 60 FPS target
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          self.running = False
+
+      #update
+      self.all_sprites.update(dt)
+
+      #draw
+      self.display_surface.fill(COLORS['bg'])
+      self.all_sprites.draw(self.display_surface)
+      pygame.display.update()
+    pygame.quit()
+
+if __name__ == '__main__':
+  game = Game()
+  game.run()
---
+++ sprites.py
@@ -0,0 +1,66 @@
+from settings import *
+from random import choice, uniform
+
+
+class Player(pygame.sprite.Sprite):
+  def __init__(self, groups):
+    super().__init__(groups)
+
+    # image
+    self.image = pygame.Surface(SIZE['paddle'], pygame.SRCALPHA)
+    pygame.draw.rect(self.image, COLORS['paddle'], pygame.Rect((0,0), SIZE['paddle']), 0, 5)
+
+    # rect
+    self.rect = self.image.get_rect(center = POS['player'])
+    self.direction = 0
+    self.speed = SPEED['player']
+
+  def move(self, dt):
+    self.rect.centery += self.direction * self.speed * dt
+    self.rect.top = 0 if self.rect.top < 0 else self.rect.top
+    self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
+
+  def get_direction(self):
+    keys = pygame.key.get_pressed()
+    self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+  def update(self, dt):
+    self.get_direction()
+    self.move(dt)
+
+class Ball(pygame.sprite.Sprite):
+  def __init__(self, groups, paddle_sprites):
+    super().__init__(groups)
+
+    #image
+    self.image = pygame.Surface(SIZE['ball'], pygame.SRCALPHA)
+    pygame.draw.circle(self.image, COLORS['ball'], (SIZE['ball'][0] / 2,SIZE['ball'][1] / 2), SIZE['ball'][0] / 2)
+
+    #rect & movement
+    self.rect = self.image.get_rect(center =(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+    self.direction = pygame.Vector2(choice((1,-1)), uniform(0.7,0.8) * choice((-1,1)))
+
+
+  def move(self, dt):
+    self.rect.center += self.direction * SPEED['ball'] * dt
+
+  def wall_collision(self):
+    if self.rect.top <= 0:
+      self.rect.top = 0
+      self.direction.y *= -1
+
+    if self.rect.bottom >= WINDOW_HEIGHT:
+      self.rect.bottom = WINDOW_HEIGHT
+      self.direction.y *= -1
+
+    if self.rect.right >= WINDOW_WIDTH:
+      self.rect.right = WINDOW_WIDTH
+      self.direction.x *= -1
+
+    if self.rect.left <= 0:
+      self.rect.left = 0
+      self.direction.x *= -1
+
+  def update(self, dt):
+    self.move(dt)
+    self.wall_collision()
