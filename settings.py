9c3f114b1a7b19bc2cab22dd15aad2424f4b489d
theblkguy <allblk13@gmail.com>
2025-08-06 21:50:24 -0500

    Add/ Opponent paddle and ball collision physics

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ from os.path import join
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 SIZE = {'paddle': (40,100), 'ball': (30,30)}
-POS = { 'player': (WINDOW_WIDTH - 50, WINDOW_HEIGHT // 2), 'opponent' : (50, WINDOW_HEIGHT // 2)}
-SPEED = {'player': 500,'opponent': 250, 'ball': 450}
+POS = { 'player': (WINDOW_WIDTH - 50, WINDOW_HEIGHT // 2), 'player2' : (50, WINDOW_HEIGHT // 2)}
+SPEED = {'player': 500,'player2': 250, 'ball': 450}
 COLORS = {
   'paddle' : "#ee322c",
   'paddle shadow' : "#b12521",
