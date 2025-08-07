ceb3dd635946cfc5a7d68a0bc458d0462554f15a
theblkguy <allblk13@gmail.com>
2025-08-07 13:04:16 -0500

    Add/ Score and ball speed now working as intended

--- settings.py
+++ settings.py
@@ -4,11 +4,20 @@ from os.path import join
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 SIZE = {'paddle': (40,100), 'ball': (30,30)}
 POS = { 'player': (WINDOW_WIDTH - 50, WINDOW_HEIGHT // 2), 'player2' : (50, WINDOW_HEIGHT // 2)}
-SPEED = {'player': 500,'player2': 500, 'ball': 450}
+SPEED = {'player': 500,'player2': 500, 'ball': 250}
 COLORS = {
   'paddle' : "#ee322c",
   'paddle shadow' : "#b12521",
   'ball' : "#ee622c",
   'ball shadow': "#c14f24",
-  'bg' : "#002633"
+  'bg' : "#002633",
+  'center_line': "#ffffff80",  # Semi-transparent white
+  'score': "#ffffff"
+}
+
+# Score settings
+SCORE_FONT_SIZE = 72
+SCORE_POS = {
+  'player1': (WINDOW_WIDTH // 4 * 3, 100),  # Right side
+  'player2': (WINDOW_WIDTH // 4, 100)       # Left side
 }
